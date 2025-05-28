db5073a7d8ef74c1209b05f17f373b7fe2eea129
anilkharatmol <akharatmol8@gmail.com>
2025-05-28 15:19:12 +0530

    Small change

--- src/pages/AppLayout.jsx
+++ src/pages/AppLayout.jsx
@@ -1,11 +1,9 @@
 import Map from "../components/Map";
 import Sidebar from "../components/Sidebar";
 import User from "../components/User";
-import { useAuth } from "../context/FakeAuthContext";
 import styles from './AppLayout.module.css'
 
 export default function AppLayout () {
-  const {isAuthenticated} = useAuth
   return (
     <div className={styles.app}>
         <Sidebar/>
